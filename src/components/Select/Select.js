d1db1462b12737ccf03b1f6c1a2a7dd8f56789d5
Rebecca Hirai <63275270+rebecca1231@users.noreply.github.com>
2021-10-31 14:34:33 -0400

    select solution

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,26 +9,56 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children)
 
   return (
-    <SelectElement value={value} onChange={onChange}>
-      {children}
-    </SelectElement>
+    <Wrapper>
+      <SelectElement value={value} onChange={onChange}>
+        {children}
+      </SelectElement>
+      <Displayed>
+        {displayedValue}
+        <IconWrapper style={{ '--size': 24 + 'px' }}>
+          <Icon id='chevron-down' strokeWidth={1} size={24}></Icon>
+        </IconWrapper>
+      </Displayed>
+    </Wrapper>
   )
 }
-
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
 const SelectElement = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+`
+const Displayed = styled.div`
   border-radius: 8px;
   padding: 12px 16px;
+  padding-right: 52px;
   background-color: ${COLORS.transparentGray15};
   color: ${COLORS.gray700};
-  min-width: 190px;
-  border: none;
-  &:hover{
-    color: #000;
-  }
-  &:focus{
-    outline: 2px solid #4374CB;
 
+  ${SelectElement}:focus + & {
+    outline: 1px dotted #121212;
+    outline: 5px auto -webkit-focus-ring-color;
   }
+
+  ${SelectElement}:hover + & {
+    color: ${COLORS.black};
+  }
+`
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 10px;
+  bottom: 0;
+  width: var(--size);
+  height: var(--size);
+  margin: auto;
+  pointer-events: none;
 `
 
 export default Select
